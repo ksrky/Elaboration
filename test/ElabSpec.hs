d0667a7a8ba50f0c17a6e42c8129b0929f751356
ksrky <xgrk9475@gmail.com>
2024-04-05 00:29:29 +0900

    update stack version

--- test/ElabSpec.hs
+++ test/ElabSpec.hs
@@ -12,5 +12,5 @@ spec :: Spec
 spec = do
     describe "eval" $ do
         it "λx → x" $
-            (runElabM (infer (Raw.Lam "x" (Right Expl) (Raw.Var "x"))) =<< initElabCtx)
-                `shouldReturn` (Lam "x" Expl (Var 0), VPi "x" Impl VU (Closure (Env.fromList [VVar 0]) (Arrow (Var 1) (Var 1))))
+            (runElabM (infer (Raw.Lam "a" (Right Impl) (Raw.Lam "x" (Right Expl) (Raw.Var "x")))) =<< initElabCtx)
+                `shouldReturn` (Lam "x" Expl (Var 0), VPi "a" Impl VU (Closure (Env.fromList [VVar 0]) (Arrow (Var 1) (Var 1))))
