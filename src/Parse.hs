bfb9341d8e8cb3ac7891e2a12adf6e82757e62e6
ksrk <xgrk9475@gmail.com>
2024-04-13 20:37:32 +0900

    feat: multiple parsers for a same token

--- src/Parse.hs
+++ src/Parse.hs
@@ -13,10 +13,13 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Data.Map.Strict      qualified as M
 
+newtype Name = Name String
+    deriving (Eq, Show)
+
 type BindingPower = Int
 
 data OpParser t = OpParser
-    { name     :: String
+    { name     :: Name
     , symbols  :: [t]
     , bindPows :: [BindingPower]
     }
@@ -25,11 +28,13 @@ type LeadingOpParser = OpParser
 type TrailingOpParser = OpParser
 
 data OpTable t = OpTable
-    { leadingOps  :: M.Map t (LeadingOpParser t)
-    , trailingOps :: M.Map t (TrailingOpParser t)
+    { leadingOps  :: M.Map t [LeadingOpParser t]
+    , trailingOps :: M.Map t [TrailingOpParser t]
     }
 
 type ParserM t = ReaderT (OpTable t) (StateT [t] (Except String))
+
+type ParserFn t = OpParser t -> ParserM t Syntax
 
 runParserM :: ParserM t a -> OpTable t -> [t] -> IO a
 runParserM m table toks = case runExcept (evalStateT (runReaderT m table) toks) of
@@ -67,20 +72,21 @@ matchToken p = do
         then return ()
         else throwError "tokens unmatched"
 
-getLeadingOpParser :: Ord t => t -> ParserM t (Maybe (LeadingOpParser t))
-getLeadingOpParser tok = do
+getLeadingOpParsers :: Ord t => t -> ParserM t [LeadingOpParser t]
+getLeadingOpParsers tok = do
     lops <- asks leadingOps
-    return $ M.lookup tok lops
+    return $ concat $ M.lookup tok lops
 
-getTrailingOpParser :: Ord t => t -> ParserM t (Maybe (LeadingOpParser t))
-getTrailingOpParser tok = do
-    lops <- asks trailingOps
-    return $ M.lookup tok lops
+getTrailingOpParsers :: Ord t => t -> ParserM t [TrailingOpParser t]
+getTrailingOpParsers tok = do
+    tops <- asks trailingOps
+    return $ concat $ M.lookup tok tops
 
-data Syntax = Node String [Syntax] | Atom String | Missing deriving (Eq, Show)
+data Syntax = Node Name [Syntax] | Atom String
+    deriving (Eq, Show)
 
-parseAtom :: Token t => t -> ParserM t Syntax
-parseAtom inp = return $ Atom $ tokenString inp
+mkAtom :: Token t => t -> Syntax
+mkAtom inp = Atom $ tokenString inp
 
 parseOpRest :: Token t => BindingPower -> [t] -> [BindingPower] -> ParserM t [Syntax]
 parseOpRest curbp _ (bp : _) | bp < curbp = throwError "lower bp"
@@ -93,46 +99,54 @@ parseOpRest curbp (sym : syms) (bp : bps) = do
     (stx :) <$> parseOpRest curbp syms bps
 parseOpRest _ _ [] = return []
 
+tryParsers :: forall t. Syntax -> [OpParser t] -> ParserFn t -> ParserM t Syntax
+tryParsers def parsers parserFn = go parsers
+  where
+    go :: [OpParser t] -> ParserM t Syntax
+    go [] = return def
+    go (parser : rest) = do
+        toks <- get
+        parserFn parser `catchError` (\_ -> put toks >> go rest)
+
 parseLeading :: Token t => BindingPower -> ParserM t Syntax
 parseLeading curbp = do
     tok <- nextToken
-    mb_parser <- getLeadingOpParser tok
-    lhs <- case mb_parser of
-        Nothing -> parseAtom tok
-        Just parser -> do
+    parsers <- getLeadingOpParsers tok
+    lhs <- case parsers of
+        [] -> return $ mkAtom tok
+        _ -> tryParsers (mkAtom tok) parsers $ \parser -> do
             stxs <- parseOpRest curbp (tail parser.symbols) parser.bindPows
             return $ Node parser.name stxs
-        `catchError` (\_ -> parseAtom tok) -- TODO: multimap, backtrack
     parseTrailing curbp lhs
 
 parseTrailing :: Token t => BindingPower -> Syntax -> ParserM t Syntax
 parseTrailing curbp lhs = do
     tok <- peekToken
-    mb_parser <- getTrailingOpParser tok
-    case mb_parser of
-        Nothing     -> return lhs
-        Just parser -> do
+    parsers <- getTrailingOpParsers tok
+    case parsers of
+        [] -> return lhs
+        _ -> tryParsers (mkAtom tok) parsers $ \parser -> do
             when (head parser.bindPows < curbp) $ throwError "lower bp"
             nextToken_
             stxs <- parseOpRest curbp (tail parser.symbols) (tail parser.bindPows)
             let lhs' = Node parser.name (lhs : stxs)
             parseTrailing curbp lhs'
-    `catchError` (\_ -> return lhs) -- TODO: multimap, backtrack
+    `catchError` (\_ -> return lhs)
 
 parse :: Token t => ParserM t Syntax
 parse = parseLeading 0
 
 sampleOpTable :: OpTable String
 sampleOpTable = OpTable
-    { leadingOps   = M.fromList
-        [ ("-", OpParser{name = "Minus", symbols = ["-"], bindPows = [75]})
+    { leadingOps = M.fromList
+        [ ("-", [OpParser{name = Name "Minus", symbols = ["-"], bindPows = [75]}])
         ]
     , trailingOps = M.fromList
-        [ ("+", OpParser{name="Add", symbols=["+"], bindPows = [65, 66]})
-        , ("-", OpParser{name="Sub", symbols=["-"], bindPows = [65, 66]})
-        , ("*", OpParser{name="Mul", symbols=["*"], bindPows = [70, 71]})
+        [ ("+", [OpParser{name = Name "Add", symbols = ["+"], bindPows = [65, 66]}])
+        , ("-", [OpParser{name = Name "Sub", symbols = ["-"], bindPows = [65, 66]}])
+        , ("*", [OpParser{name = Name "Mul", symbols = ["*"], bindPows = [70, 71]}])
         ]
     }
 
 test :: IO Syntax
-test = runParserM parse sampleOpTable ["1", "+", "2", "+", "3"]
+test = runParserM parse sampleOpTable ["1", "+", "2", "*", "-", "3"]
