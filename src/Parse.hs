6e09b6c3612fe67691c852c10e574d35bdb00dee
ksrk <xgrk9475@gmail.com>
2024-04-14 12:00:22 +0900

    feat: backtrack with logict

--- src/Parse.hs
+++ src/Parse.hs
@@ -8,7 +8,9 @@ module Parse
     , test
     ) where
 
+import Control.Applicative
 import Control.Monad.Except
+import Control.Monad.Logic
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.List            qualified as L
@@ -42,15 +44,28 @@ data OpTable t = OpTable
     , trailingOps :: M.Map t [TrailingOpParser t]
     }
 
+-- | Parser logic monad.
+type ParserLogicM t m = StateT [t] (LogicT m)
+
+runParserLogicM :: Applicative m => ParserLogicM t m a -> [t] -> m [(a, [t])]
+runParserLogicM m toks = observeAllT (runStateT m toks)
+
+evalParserLogicM :: Applicative m => ParserLogicM t m a -> [t] -> m [a]
+evalParserLogicM m toks = observeAllT (evalStateT m toks)
+
+liftParserLogicM :: Monad m => m a -> ParserLogicM t m a
+liftParserLogicM = lift . lift
+
 -- | Parser monad has operator table, token stream, and error handling.
-type ParserM t = ReaderT (OpTable t) (StateT [t] (Except String))
+type ParserM t = ParserLogicM t (ReaderT (OpTable t) (Except String))
+
+runParserM :: ParserM t a -> [t] -> OpTable t -> IO a
+runParserM m toks table = case runExcept (runReaderT (evalParserLogicM m toks) table) of
+    Left msg        -> putStrLn msg >> error "unexpected"
+    Right []        -> error "no answer"
+    Right (res : _) -> return res
 
 type ParserFn t a = OpParser t -> ParserM t a
-
-runParserM :: ParserM t a -> OpTable t -> [t] -> IO a
-runParserM m table toks = case runExcept (evalStateT (runReaderT m table) toks) of
-    Left msg  -> putStrLn msg >> error "unexpected"
-    Right res -> return res
 
 class (Show t, Ord t) => Token t where
     tokenString :: t -> String
@@ -83,14 +98,20 @@ matchToken p = do
         then return ()
         else throwError "tokens unmatched"
 
-tryParsers :: forall t a. a -> [OpParser t] -> ParserFn t a -> ParserM t a
-tryParsers def parsers parserFn = go parsers
-  where
-    go :: [OpParser t] -> ParserM t a
-    go [] = return def
-    go (parser : rest) = do
-        toks <- get
-        parserFn parser `catchError` (\_ -> put toks >> go rest)
+longestMatch :: ParserM t a -> ParserM t a
+longestMatch m = do
+    toks <- get
+    xtoks <- liftParserLogicM $ runParserLogicM m toks
+    when (null xtoks) $ throwError "no parsers"
+    let (x, toks') = L.minimumBy (\(_, ts1) (_, ts2) -> compare (length ts1) (length ts2)) xtoks
+    put toks'
+    return x
+
+tryParsers :: [OpParser t] -> ParserFn t a -> ParserM t a
+tryParsers [] _ = throwError "no parsers"
+tryParsers parsers parserFn = do
+    parser <- foldr ((<|>) . pure) empty parsers
+    parserFn parser `catchError` const empty
 
 getLeadingOpParsers :: Token t => t -> ParserM t [LeadingOpParser t]
 getLeadingOpParsers tok = do
@@ -133,25 +154,23 @@ parseLeading :: Token t => BindingPower -> ParserM t Syntax
 parseLeading bp = do
     tok <- nextToken
     parsers <- getLeadingOpParsers tok
-    lhs <- case parsers of
-        [] -> return $ mkAtom tok
-        _ -> tryParsers (mkAtom tok) parsers $ \parser -> do
+    lhs <- do
+        longestMatch $ tryParsers parsers $ \parser -> do
             stxs <- parseOpRest (tail parser.symbols) parser.bindPows
             return $ Node parser.name stxs
+        `catchError` (\_ -> return $ mkAtom tok)
     parseTrailing bp lhs
 
 parseTrailing :: Token t => BindingPower -> Syntax -> ParserM t Syntax
 parseTrailing bp lhs = do
     tok <- peekToken
     parsers <- getTrailingOpParsers tok
-    case parsers of
-        [] -> return lhs
-        _ -> tryParsers (mkAtom tok) parsers $ \parser -> do
-            when (head parser.bindPows < bp) $ throwError "lower bp"
-            nextToken_
-            stxs <- parseOpRest (tail parser.symbols) (tail parser.bindPows)
-            let lhs' = Node parser.name (lhs : stxs)
-            parseTrailing bp lhs'
+    longestMatch $ tryParsers parsers $ \parser -> do
+        when (head parser.bindPows < bp) $ throwError "lower bp"
+        nextToken_
+        stxs <- parseOpRest (tail parser.symbols) (tail parser.bindPows)
+        let lhs' = Node parser.name (lhs : stxs)
+        parseTrailing bp lhs'
     `catchError` (\_ -> return lhs)
 
 parse :: Token t => ParserM t Syntax
@@ -160,23 +179,25 @@ parse = parseLeading 0
 sampleOpTable :: OpTable String
 sampleOpTable = OpTable
     { leadingOps = M.fromList
-        [ ("-", [OpParser{name = Name "Minus", symbols = ["-"], bindPows = [75]}])
-        , ("if", [OpParser{name = Name "IfThenElse", symbols = ["if", "then", "else"], bindPows = [30, 30, 30]}])
-        , ("(", [OpParser{name = Name "Paren", symbols = ["(", ")"], bindPows = [0]}])
+        [ ("-",  [OpParser{name = Name "Minus", symbols = ["-"], bindPows = [75]}])
+        , ("if", [ OpParser{name = Name "IfThenElse", symbols = ["if", "then", "else"], bindPows = [30, 30, 30]}
+                 , OpParser{name = Name "IfThen", symbols = ["if", "then"], bindPows = [30, 30]}
+                 ])
+        , ("(",  [OpParser{name = Name "Paren", symbols = ["(", ")"], bindPows = [0]}])
         ]
     , trailingOps = M.fromList
         [ ("||", [OpParser{name = Name "Or", symbols = ["||"], bindPows = [30, 31]}])
         , ("&&", [OpParser{name = Name "And", symbols = ["&&"], bindPows = [35, 36]}])
         , ("==", [OpParser{name = Name "Eq", symbols = ["=="], bindPows = [50, 51]}])
-        , ("+", [OpParser{name = Name "Add", symbols = ["+"], bindPows = [65, 66]}])
-        , ("-", [OpParser{name = Name "Sub", symbols = ["-"], bindPows = [65, 66]}])
-        , ("*", [OpParser{name = Name "Mul", symbols = ["*"], bindPows = [70, 71]}])
-        , ("[", [OpParser{name = Name "Subscript", symbols = ["[", "]"], bindPows = [100, 0]}])
+        , ("+",  [OpParser{name = Name "Add", symbols = ["+"], bindPows = [65, 66]}])
+        , ("-",  [OpParser{name = Name "Sub", symbols = ["-"], bindPows = [65, 66]}])
+        , ("*",  [OpParser{name = Name "Mul", symbols = ["*"], bindPows = [70, 71]}])
+        , ("[",  [OpParser{name = Name "Subscript", symbols = ["[", "]"], bindPows = [100, 0]}])
         ]
     }
 
 test :: IO Syntax
-test = runParserM parse sampleOpTable ["if", "1", "==", "2", "then", "3", "+", "4", "else", "5"]
+test = runParserM parse ["if", "1", "==", "2", "then", "3", "+", "4", "else", "5"] sampleOpTable
 
 -- ["1", "+", "2", "*", "-", "3"]
 -- ["(", "1", "+", "2", ")", "*", "3"]
